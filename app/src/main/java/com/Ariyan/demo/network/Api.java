fa55ad9b7093301d23a1c9700e0e54ea0f9e18c6
ripat123 <ripatrabbi2@outlook.com>
2024-07-14 18:07:58 +0600

    14-07-2024

--- app/src/main/java/com/Ariyan/demo/network/Api.java
+++ app/src/main/java/com/Ariyan/demo/network/Api.java
@@ -1,11 +1,17 @@
 package com.Ariyan.demo.network;
 
+import java.util.Map;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface Api {
-    @GET("/users/{type}")
+    @POST("/users/{type}")
     Call<String> getLoginID(@Path("type") String type, @Path("delivery_id") String Did,
                             @Path("dob") String date, String token);
+    @POST("")
+    Call<String> UpdateToken(@Body Map<String, String> map);
 }
